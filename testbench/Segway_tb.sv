c09be2e99f1d08fa3b2fc1ca2441496e1cbeed39
ASxx <oliver12125@sina.com>
2022-12-04 05:30:31 +0800

    OOP features

--- testbench/Segway_tb.sv
+++ testbench/Segway_tb.sv
@@ -1,24 +1,35 @@
+interface Seg_ports();          // external interaction with Segway 
+      logic [7:0] cmd;				// command host is sending to DUT
+      logic send_cmd;				// asserted to initiate sending of command
+      logic signed [15:0] rider_lean;
+      logic [11:0] ld_cell_lft, ld_cell_rght,steerPot,batt;	// A2D values
+      logic OVR_I_lft, OVR_I_rght;
+      //check the output
+      logic cmd_sent;
+endinterface
+// !!! Because package would be compiled in the module part. Then this
+// real interface would be conflexed with the Vif defined in the class
+// if it is local def. So set this interface as a global one.
+
+
 module Segway_tb();
-			
+
+import tb_tasks::*;
 //// Interconnects to DUT/support defined as type wire /////
 wire SS_n,SCLK,MOSI,MISO,INT;				// to inertial sensor
 wire A2D_SS_n,A2D_SCLK,A2D_MOSI,A2D_MISO;	// to A2D converter
 wire RX_TX;
 wire PWM1_rght, PWM2_rght, PWM1_lft, PWM2_lft;
 wire piezo,piezo_n;
-wire cmd_sent;
 wire rst_n;					// synchronized global reset
 
+
 ////// Stimulus is declared as type reg ///////
+
 reg clk, RST_n;
-reg [7:0] cmd;				// command host is sending to DUT
-reg send_cmd;				// asserted to initiate sending of command
-reg signed [15:0] rider_lean;
-reg [11:0] ld_cell_lft, ld_cell_rght,steerPot,batt;	// A2D values
-reg OVR_I_lft, OVR_I_rght;
 
 ///// Internal registers for testing purposes??? /////////
-
+Seg_ports Seg_intf();     // use Vif-->IF to bridge between Seg_drv class and DUT
 
 ////////////////////////////////////////////////////////////////
 // Instantiate Physical Model of Segway with Inertial sensor //
@@ -26,25 +37,25 @@ reg OVR_I_lft, OVR_I_rght;
 SegwayModel iPHYS(.clk(clk),.RST_n(RST_n),.SS_n(SS_n),.SCLK(SCLK),
                   .MISO(MISO),.MOSI(MOSI),.INT(INT),.PWM1_lft(PWM1_lft),
 				  .PWM2_lft(PWM2_lft),.PWM1_rght(PWM1_rght),
-				  .PWM2_rght(PWM2_rght),.rider_lean(rider_lean));				  
+				  .PWM2_rght(PWM2_rght),.rider_lean(Seg_intf.rider_lean));				  
 
 /////////////////////////////////////////////////////////
 // Instantiate Model of A2D for load cell and battery //
 ///////////////////////////////////////////////////////
 ADC128S_FC iA2D(.clk(clk),.rst_n(RST_n),.SS_n(A2D_SS_n),.SCLK(A2D_SCLK),
-             .MISO(A2D_MISO),.MOSI(A2D_MOSI),.ld_cell_lft(ld_cell_lft),.ld_cell_rght(ld_cell_rght),
-			 .steerPot(steerPot),.batt(batt));			
+             .MISO(A2D_MISO),.MOSI(A2D_MOSI),.ld_cell_lft(Seg_intf.ld_cell_lft),.ld_cell_rght(Seg_intf.ld_cell_rght),
+			 .steerPot(Seg_intf.steerPot),.batt(Seg_intf.batt));			
 	 
 ////// Instantiate DUT ////////
 Segway #(.fast_sim(1)) iDUT(.clk(clk),.RST_n(RST_n),.INERT_SS_n(SS_n),.INERT_MOSI(MOSI),
             .INERT_SCLK(SCLK),.INERT_MISO(MISO),.INERT_INT(INT),.A2D_SS_n(A2D_SS_n),
 			.A2D_MOSI(A2D_MOSI),.A2D_SCLK(A2D_SCLK),.A2D_MISO(A2D_MISO),
 			.PWM1_lft(PWM1_lft),.PWM2_lft(PWM2_lft),.PWM1_rght(PWM1_rght),
-			.PWM2_rght(PWM2_rght),.OVR_I_lft(OVR_I_lft),.OVR_I_rght(OVR_I_rght),
+			.PWM2_rght(PWM2_rght),.OVR_I_lft(Seg_intf.OVR_I_lft),.OVR_I_rght(Seg_intf.OVR_I_rght),
 			.piezo_n(piezo_n),.piezo(piezo),.RX(RX_TX));
 
 //// Instantiate UART_tx (mimics command from BLE module) //////
-uart_tx iTX(.clk(clk),.rst_n(rst_n),.TX(RX_TX),.trmt(send_cmd),.tx_data(cmd),.tx_done(cmd_sent));
+uart_tx iTX(.clk(clk),.rst_n(rst_n),.TX(RX_TX),.trmt(Seg_intf.send_cmd),.tx_data(Seg_intf.cmd),.tx_done(Seg_intf.cmd_sent));
 
 /////////////////////////////////////
 // Instantiate reset synchronizer //
@@ -52,42 +63,30 @@ uart_tx iTX(.clk(clk),.rst_n(rst_n),.TX(RX_TX),.trmt(send_cmd),.tx_data(cmd),.tx
 rst_synch iRST(.clk(clk),.RST_n(RST_n),.rst_n(rst_n));
 
 initial begin
-  
+  Seg_drv seg_drv;
+  seg_drv = new(Seg_intf);
+  seg_drv.init();
   /// Your magic goes here ///
   clk = 0;
   RST_n = 0;
   repeat(3) @(negedge clk);
   RST_n = 1;
-  send_cmd = 0;
   repeat(3) @(negedge clk);
-  rider_lean = 0;
-  {OVR_I_lft, OVR_I_rght} = {0,0};
-
   // send "g" to segway
-  uart_tx_case(8'h67);
-  {ld_cell_lft, ld_cell_rght,steerPot,batt} = {12'd400, 12'd300, 12'd200, 12'h8FF};
-  //{ld_cell_lft, ld_cell_rght,steerPot,batt} = {12'd0, 12'd0, 12'd0, 12'h0};
+  seg_drv.uart_tx_case(.clk(clk), .tx_in(8'h67));
+  seg_drv.rider_config(12'd400, 12'd300, 12'd200, 12'h8FF);
+
   wait(iDUT.pwr_up == 1);
+  $display("the Segway has powered up");
   repeat(300000) @(posedge clk);
-  rider_lean = 16'h0fff;
+  seg_drv.vif.rider_lean = 16'h0;
   repeat(800000) @(posedge clk);
-  rider_lean = 16'd0;
+  seg_drv.vif.rider_lean = 16'd0;
   repeat(800000) @(posedge clk);
   $stop();
 end
 
 always
   #10 clk = ~clk;
-  
 
-task uart_tx_case(input[7:0] tx_in);
-		@(negedge clk);
-		send_cmd = 1;
-		cmd = tx_in;
-		@(negedge clk);
-		send_cmd = 0;		// 1clk trmt, to capture and set txdata;
-		wait(cmd_sent == 1'b1); 	// serial data done, otherwise uart_tx is not sent
-		$display("tx data, %h, is sent",tx_in); 
-endtask : uart_tx_case
-
-endmodule
+endmodule	
--- testbench/tb_tasks.sv
+++ testbench/tb_tasks.sv
@@ -1,7 +1,41 @@
 package tb_tasks;
+      // https://stackoverflow.com/questions/67922669/what-causes-interface-resolution-compilation-error-when-working-with-classes-a
+      class Seg_drv;
 
-task Seg_initial(input clk);
-	
-endtask : Seg_initial
+            virtual Seg_ports vif; // virtual interface link to segway
+
+            function new( virtual Seg_ports s);
+                  vif = s; // initialize the virtual interface 
+            endfunction
+
+            task init();   //init the stimulus
+                  {vif.OVR_I_lft, vif.OVR_I_rght} = {1'b0, 1'b0};
+                  {vif.ld_cell_lft, vif.ld_cell_rght,vif.steerPot,vif.batt} 
+                  = {'0, '0, '0, '0};
+                  vif.cmd = '0;
+                  vif.send_cmd = '0;
+                  vif.rider_lean = '0;
+            endtask
+
+            task automatic uart_tx_case(input[7:0] tx_in, ref clk);
+                  @(negedge clk);
+                  vif.send_cmd = 1;
+                  vif.cmd = tx_in;
+                  @(negedge clk);
+                  vif.send_cmd = 0;		// 1clk trmt, to capture and set txdata;
+                  wait(vif.cmd_sent == 1'b1); 	// serial data done, otherwise uart_tx is not sent
+                  $display("tx data, %h, is sent",tx_in);   
+            endtask
+
+            task rider_config(input[11:0] lcl, input[11:0] lcr, input[11:0] sp, input[11:0] bt);
+                 {vif.ld_cell_lft, vif.ld_cell_rght, vif.steerPot, vif.batt} = 
+                 {lcl, lcr, sp, bt}; 
+            endtask
+
+            task random_leaning(input[15:0] Lowl, input[15:0] Highl);
+                  vif.rider_lean = $urandom(Lowl, Highl);
+            endtask
+
+      endclass
 
 endpackage : tb_tasks
